b77f58622c14bb0ed0de34a79b8d5a70f0591a80
大胖子 <945541696@qq.com>
2022-07-10 20:25:38 +0800

    搞不定

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,28 +1,28 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  $("head").prepend(
-    `<style>
-       .slide-image {
-          height: auto;
-          width: 100vw;
-        }
-      </style> `
-  );
-  $("body").prepend(
-    `<img  src="${request.url}" id="${request.imageDivId}" 
-           class="slide-image" /> `
-  );
-  $(`#${request.imageDivId}`).click(function () {
-    $(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
-  });
-  sendResponse({ fromcontent: "This message is from content.js" });
-});
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   $("head").prepend(
+//     `<style>
+//        .slide-image {
+//           height: auto;
+//           width: 100vw;
+//         }
+//       </style> `
+//   );
+//   $("body").prepend(
+//     `<img  src="${request.url}" id="${request.imageDivId}" 
+//            class="slide-image" /> `
+//   );
+//   $(`#${request.imageDivId}`).click(function () {
+//     $(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
+//   });
+//   sendResponse({ fromcontent: "This message is from content.js" });
+// });
 
-function queryInputDom(title) {
-  const formItem = document.querySelector(
-    `.ant-modal-body .ant-form-item-label>label[title="${title}"]`
-  )?.parentNode.parentNode;
-  const input =
-    formItem &&
-    formItem.querySelector(".ant-form-item-control-wrapper .ant-input");
-  return input;
-}
+// function queryInputDom(title) {
+//   const formItem = document.querySelector(
+//     `.ant-modal-body .ant-form-item-label>label[title="${title}"]`
+//   )?.parentNode.parentNode;
+//   const input =
+//     formItem &&
+//     formItem.querySelector(".ant-form-item-control-wrapper .ant-input");
+//   return input;
+// }
