7bd0a35f368d5e41bc3cb2809b53133282e4edda
tangky <945541696@qq.com>
2022-07-12 19:01:56 +0800

    简单填充

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,28 +1,26 @@
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   $("head").prepend(
-//     `<style>
-//        .slide-image {
-//           height: auto;
-//           width: 100vw;
-//         }
-//       </style> `
-//   );
-//   $("body").prepend(
-//     `<img  src="${request.url}" id="${request.imageDivId}" 
-//            class="slide-image" /> `
-//   );
-//   $(`#${request.imageDivId}`).click(function () {
-//     $(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
-//   });
-//   sendResponse({ fromcontent: "This message is from content.js" });
-// });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  //维护title列表:税号 客户名称 etc
+  const inputTypes = ["客户名称", "联系人", "税号", "联系电话"];
+  // 根据title获取dom中相应的输入框;todo:单选,多选等
+  const inputs = [];
+  for (let index = 0; index < inputTypes.length; index++) {
+    const element = queryInputDom(inputTypes[index]);
+    if (element) {
+      element.value = inputTypes[index] + Math.random();
+    }
+  }
+  // 填充随机数,根据字段格式填充
+  // const taxnum = queryInputDom("税号");
+  // taxnum.value = "1232142132";
+  sendResponse({ fromcontent: "This message is from content.js" });
+});
 
-// function queryInputDom(title) {
-//   const formItem = document.querySelector(
-//     `.ant-modal-body .ant-form-item-label>label[title="${title}"]`
-//   )?.parentNode.parentNode;
-//   const input =
-//     formItem &&
-//     formItem.querySelector(".ant-form-item-control-wrapper .ant-input");
-//   return input;
-// }
+function queryInputDom(title) {
+  const formItem = document.querySelector(
+    `.ant-modal-body .ant-form-item-label>label[title="${title}"]`
+  )?.parentNode.parentNode;
+  const input =
+    formItem &&
+    formItem.querySelector(".ant-form-item-control-wrapper .ant-input");
+  return input;
+}
